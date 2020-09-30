59373a879c6f85be92b173fd8133a6c1b26665fd
John Pepp <jrpepp@pepster.com>
2020-09-29 20:45:53 -0400

    Everyday should now have 10 questions

--- setupQuestions.php
+++ setupQuestions.php
@@ -3,12 +3,26 @@
 require_once 'assets/config/config.php';
 require_once "vendor/autoload.php";
 
-use Miniature\Trivia;
+//use Miniature\Trivia;
+//
+//$trivia = new Trivia();
+//
+//$trivia::updateYear();
+//$data = $trivia::read();
+//
+//echo "<pre>" . print_r($data, 1) . "</pre>";
 
-$trivia = new Trivia();
+$total = 23;
+$max = 10;
+$max_offset = floor($total / $max) * $max;
 
-$trivia->resetPlaydate();
-$data = $trivia::read();
+echo $max_offset . "<br>";
+echo $total % $max;
+echo "<br>";
+$new_offset = $total - ($max - ($total % $max));
 
-echo "<pre>" . print_r($data, 1) . "</pre>";
+echo $new_offset ."<br>";
+
+
+
 
--- src/Trivia.php
+++ src/Trivia.php
@@ -8,7 +8,7 @@ use Miniature\Database as DB;
 class Trivia {
 
     public $schedule = null;
-    
+
     static protected function pdo() {
         $db = DB::getInstance();
         $pdo = $db->getConnection();
@@ -23,11 +23,27 @@ class Trivia {
     }
 
     static public function read() {
-        $query = 'SELECT id FROM trivia_questions WHERE hidden=:hidden';
+        $query = 'SELECT * FROM trivia_questions WHERE hidden=:hidden';
         $stmt = static::pdo()->prepare($query); // Prepare the query:
-        $stmt->execute([':hidden' => 'yes']); // Execute the query with the supplied user's parameter(s):
+        $stmt->execute([':hidden' => 'no']); // Execute the query with the supplied user's parameter(s):
         $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
         return $data;
+    }
+
+    static public function updateYear() {
+        $days = new \DateTime("now", new \DateTimeZone("America/Detroit"));
+
+        $records = static::read();
+        foreach ($records as $record) {
+            $play_date = $days->format("Y-m-d");
+
+            $day_of_week = $days->format("N");
+            $day_of_year = $days->format("z");
+            $query = "UPDATE trivia_questions SET play_date=:play_date, day_of_week=:day_of_week, day_of_year=:day_of_year WHERE id=:id";
+            $stmt = static::pdo()->prepare($query);
+            $stmt->execute([':play_date' => $play_date, ':day_of_week' => $day_of_week, ':day_of_year' => $day_of_year, ':id' => $record['id']]);
+            $days->modify("+1 day");
+        }
     }
 
     public function resetPlaydate() {
@@ -38,8 +54,16 @@ class Trivia {
             $this->stmt = static::pdo()->prepare($this->query);
             $today = $this->schedule->format("Y-m-d H:i:s");
             $day_of_week = $this->schedule->format('N');
-            $this->stmt->execute([':play_date' => $today, ':day_of_week' => $day_of_week, ':id' => $record['id'] ]);
+            $this->stmt->execute([':play_date' => $today, ':day_of_week' => $day_of_week, ':id' => $record['id']]);
         }
+    }
+
+    static public function readData($max_questions, $newOffset) {
+        $query = "SELECT * FROM trivia_questions WHERE hidden=:hidden LIMIT :max_questions OFFSET :quizOffset";
+        $stmt = static::pdo()->prepare($query);
+        $stmt->execute([':hidden' => 'no', ':max_questions' => $max_questions, ':quizOffset' => $newOffset]);
+        $result = $stmt->fetchALL(PDO::FETCH_ASSOC);
+        return $result;
     }
 
 }
